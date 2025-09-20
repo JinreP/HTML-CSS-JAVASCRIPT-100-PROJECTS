40566c825d88909dcf827add10e574ef631ad773
JinreP <subeegod@gmail.com>
2025-09-20 14:11:49 +0800

    counter key events added

--- 04.counter/index.js
+++ 04.counter/index.js
@@ -7,20 +7,43 @@ const resetBtn = document.getElementById("resetBtn");
 let count = 0;
 
 function increase() {
-  count = count + 1;
+  count++;
   myLabel.textContent = count;
+  updadeColor();
 }
 
 function decrease() {
-  count = count - 1;
+  count--;
   myLabel.textContent = count;
+  updadeColor();
 }
 
 function reset() {
   count = 0;
   myLabel.textContent = count;
+  updadeColor();
+}
+function updadeColor() {
+  myLabel.textContent = count;
+
+  if (count > 0) {
+    myLabel.style.color = "green";
+  } else if (count < 0) {
+    myLabel.style.color = "red";
+  } else {
+    myLabel.style.color = "black";
+  }
 }
 
 increaseBtn.addEventListener("click", increase);
 decreaseBtn.addEventListener("click", decrease);
 resetBtn.addEventListener("click", reset);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowUp") {
+    increase();
+  } else if (event.key === "ArrowDown") {
+    decrease();
+  } else if (event.key === "r") {
+    reset();
+  }
+});
