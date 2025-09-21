9d27257f5d9476bfa635af73e37b7946f74304f9
JinreP <subeegod@gmail.com>
2025-09-21 10:58:17 +0800

    added sum to rolldice and made working digital clock

---
+++ test/index.js
@@ -0,0 +1,8 @@
+let max = 15;
+let min = 5;
+
+let randomNum1 = Math.floor(Math.random() * (max - min + 1)) + min;
+let randomNum2 = Math.floor(Math.random() * (max - min + 1)) + min;
+let randomNum3 = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomNum1 + randomNum2 + randomNum3);
